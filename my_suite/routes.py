91dcb1a7e035ffab59991f4f7e50ab3bac65c016
Coder4916 Bailey <c.bailey84@hotmail.com>
2024-02-22 22:31:44 +0000

    requirements.txt updated correctly

--- my_suite/routes.py
+++ my_suite/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template
 from my_suite import my_app, my_database
+from my_suite.models import Game, Review
 
-
-@my_app.route("/")
+@my_app.route("/base.html")
 def base():
     return render_template("base.html")
