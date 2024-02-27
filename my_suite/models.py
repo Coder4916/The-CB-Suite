e58078737083bfd22ea55117062f905c94a7769d
Coder4916 Bailey <c.bailey84@hotmail.com>
2024-02-27 16:32:56 +0000

    Project modified to understand .exe issue

--- my_suite/models.py
+++ my_suite/models.py
@@ -6,6 +6,7 @@ class Game(my_database.Model):
     id = my_database.Column(my_database.Integer, primary_key=True)
     game_name = my_database.Column(my_database.String(30), unique=True, nullable=False)
     tasks = my_database.relationship("Review", backref="game", cascade="all, delete", lazy=True)
+    
     def __repr__(self):
         # represents itself
         return self.game_name
@@ -19,6 +20,7 @@ class Review(my_database.Model):
     review = my_database.Column(my_database.Text, nullable=False)
     review_date = my_database.Column(my_database.Date, nullable=False)
     game_id = my_database.Column(my_database.Integer, my_database.ForeignKey("game.id", ondelete="CASCADE"), nullable=False)
+    
     def __repr__(self):
         # represents itself
         return "#{0} Reviewer: {1} | Review: {2} | Date: {3}".format(
--- my_suite/routes.py
+++ my_suite/routes.py
@@ -1,17 +1,28 @@
-from flask import render_template, request
+from flask import render_template, request, redirect, url_for
 from my_suite import my_app, my_database
 from my_suite.models import Review, Game
+
 
 @my_app.route("/")
 def home():
     return render_template("home.html")
 
+
+@my_app.route("/games")
+def games():
+    return render_template("games.html")
+
+
+@my_app.route("/add_game", methods=["GET", "POST"])
+def add_game():
+    if request.method == "POST":
+        game = Game(game_name=request.form.get("game_name"))
+        my_database.session.add(game)
+        my_database.session.commit()
+        return redirect(url_for("games"))
+    return render_template("add_game.html")
+
+
 @my_app.route("/reviews")
 def reviews():
     return render_template("reviews.html")
-
-@my_app.route("/add_review", methods=["GET", "POST"])
-def add_review():
-    if request.method == "POST":
-        review = Review()
-    return render_template("add_review.html")
