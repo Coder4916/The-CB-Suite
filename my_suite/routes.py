1bdeead1bac9ed8deac173f6514c8629b60f8b1c
Coder4916 Bailey <c.bailey84@hotmail.com>
2024-03-03 22:29:15 +0000

    modified several html pages to begin storing data in my_database

--- my_suite/routes.py
+++ my_suite/routes.py
@@ -9,6 +9,16 @@ def home():
     return render_template("home.html")
 
 
+@my_app.route("/add_game", methods=["GET", "POST"])
+def add_game():
+    if request.method == "POST":
+        game = Game(game_name=request.form.get("game_name"))
+        my_database.session.add(game)
+        my_database.session.commit()
+        return redirect(url_for("games"))
+    return render_template("add_game.html")
+    
+
 @my_app.route("/games")
 def games():
     data = []
@@ -29,4 +39,4 @@ def add_review():
 
 @my_app.route("/reviews")
 def reviews():
-    return render_template("review.html")
+    return render_template("reviews.html")
