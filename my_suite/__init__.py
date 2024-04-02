df8b64731de9de19fa04aa525451dad14664dec8
Coder4916 Bailey <c.bailey84@hotmail.com>
2024-04-02 20:21:02 +0000

    procfile created and __init__ file updated ready for deployment

--- my_suite/__init__.py
+++ my_suite/__init__.py
@@ -7,8 +7,18 @@ if os.path.exists("env.py"):
 
 my_app = Flask(__name__)
 my_app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-my_app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('MY_DB_URL')
+
+
+if os.environ.get("DEVELOPMENT") == "True":
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+else:
+    uri = os.environ.get("DATABASE_URL")
+    if uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+    app.config["SQLALCHEMY_DATABASE_URI"] = uri
+
 
 my_database = SQLAlchemy(my_app)
+
 
 from my_suite import routes # noqa
