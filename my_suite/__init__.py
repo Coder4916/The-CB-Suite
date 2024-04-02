0c1f49eccc4b075c7a10f1c859a3d3550a5c8613
Coder4916 Bailey <c.bailey84@hotmail.com>
2024-04-02 20:41:03 +0000

    init file updated correctly

--- my_suite/__init__.py
+++ my_suite/__init__.py
@@ -10,12 +10,12 @@ my_app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 
 
 if os.environ.get("DEVELOPMENT") == "True":
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+    my_app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 else:
     uri = os.environ.get("DATABASE_URL")
     if uri.startswith("postgres://"):
         uri = uri.replace("postgres://", "postgresql://", 1)
-    app.config["SQLALCHEMY_DATABASE_URI"] = uri
+    my_app.config["SQLALCHEMY_DATABASE_URI"] = uri
 
 
 my_database = SQLAlchemy(my_app)
